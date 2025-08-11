2536c96ee700f0b48b9e0a5359e01a1786aa8231
Kristina <kristinakovalenko333@gmail.com>
2025-08-11 23:35:04 +0300

    add localStorage support for saving recipes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import AddRecipe from "./pages/AddRecipe";
 import NavBar from "./components/NavBar";
 
 function App() {
-  const [recipes, setRecipes] = useState([]);
+  const [recipes, setRecipes] = useState(() => {
+    const savedRecipes = localStorage.getItem("recipes");
+    return savedRecipes ? JSON.parse(savedRecipes) : [];
+  });
 
   const addRecipe = (newRecipe) => {
     setRecipes((prev) => [...prev, newRecipe]);
   };
+
+  useEffect(() => {
+    localStorage.setItem("recipes", JSON.stringify(recipes));
+  }, [recipes]);
 
   return (
     <>
