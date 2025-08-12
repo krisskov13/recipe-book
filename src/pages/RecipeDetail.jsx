84b748b52e50f65a353ebf6fb5589958bb277275
Kristina <kristinakovalenko333@gmail.com>
2025-08-12 23:43:27 +0300

    add RecipeDetail page logic with URL params

--- src/pages/RecipeDetail.jsx
+++ src/pages/RecipeDetail.jsx
@@ -1,4 +1,14 @@
-function RecipeDetail() {
+import { useParams } from "react-router-dom";
+
+function RecipeDetail(recipes) {
+  const { id } = useParams;
+
+  const recipe = recipes.find((r) => r.id === Number(id));
+
+  if (!recipe) {
+    return <p>Рецепт не знайдено</p>;
+  }
+
   return (
     <div>
       <h1>Назва</h1>
