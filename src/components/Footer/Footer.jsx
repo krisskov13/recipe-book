d6a33594f5cfd197440f8660a499ce9c187219de
Kristina <kristinakovalenko333@gmail.com>
2025-08-16 21:52:20 +0300

    add a Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <footer>
+      <div>
+        <p>Контакти:</p>
+        <p>moirecepty@gmail</p>
+        <a href="tel:+380123456789">+38 012 34 56 789</a>
+      </div>
+      <div>
+        <p>© 2025 Мої Рецепти. Усі права захищено.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
