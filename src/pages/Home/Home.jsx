1c2ea091b95d86270a5e8787b19879ccfdaa693b
Kristina <kristinakovalenko333@gmail.com>
2025-08-16 19:13:54 +0300

    add recipe search by name

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,36 @@
+import { useEffect, useState } from "react";
 import RecipeCard from "../../components/RecipeCard/RecipeCard";
 import styles from "./Home.module.css";
 
 function Home({ recipes }) {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredRecipes, setFilteredRecipes] = useState(recipes);
+
+  useEffect(() => {
+    const term = searchTerm.toLowerCase();
+    const filtered = recipes.filter((recipe) =>
+      recipe.title.toLowerCase().includes(term)
+    );
+    setFilteredRecipes(filtered);
+  }, [searchTerm, recipes]);
+
   return (
     <div className={styles.container}>
       <h1>Рецепти</h1>
-      {recipes.length === 0 ? (
-        <p>Рецептів ще немає</p>
-      ) : (
+      <input
+        type="text"
+        value={searchTerm}
+        placeholder="Знайди рецепт за назвою"
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      {filteredRecipes.length > 0 ? (
         <div className={styles.listRecipes}>
-          {recipes.map((recipe) => (
+          {filteredRecipes.map((recipe) => (
             <RecipeCard key={recipe.id} recipe={recipe} />
           ))}
         </div>
+      ) : (
+        <p>Рецептів не знайдено</p>
       )}
     </div>
   );
