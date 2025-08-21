5f050198042463934bd16a6bc045e09f517a2c5b
Kristina <kristinakovalenko333@gmail.com>
2025-08-21 21:25:04 +0300

    add Login and Register pages

---
+++ src/pages/Register.jsx
@@ -0,0 +1,9 @@
+function Register() {
+  return (
+    <form>
+      <input type="text" placeholder="Введіть ім'я..." />
+      <input type="text" placeholder="Введіть пароль..." />
+      <button type="submit">Зареєструватися</button>
+    </form>
+  );
+}
