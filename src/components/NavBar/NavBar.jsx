c0d7f0cb97d69149ad91f65d56023d3a92a20372
Kristina <kristinakovalenko333@gmail.com>
2025-08-21 23:12:46 +0300

    add navigation to NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,26 @@
 import { Link } from "react-router-dom";
 import styles from "./NavBar.module.css";
+import { useAuth } from "../context/Auth";
 
 function NavBar() {
+  const { user, logout } = useAuth();
+
   return (
     <header className={styles.header}>
       <h1 className={styles.logo}>🍽 Книга рецептів</h1>
       <nav>
         <div className={styles.links}>
-          <Link to="/add">Додати рецепт</Link>
+          {user && <Link to="/add">Додати рецепт</Link>}
+        </div>
+        <div>
+          {user ? (
+            <button onClick={logout}>Вийти</button>
+          ) : (
+            <div>
+              <Link to="/login">Вхід</Link>
+              <Link to="./register">Реєстрація</Link>
+            </div>
+          )}
         </div>
       </nav>
     </header>
